9fc02d979652af7cc8ae4f0dfaf523ef72fd5492
valeriankass <lady.komarnickaya@gmail.com>
2020-05-31 23:00:35 +0200

    new header created + modified init_ijvm in machine.c

--- src/machine.c
+++ src/machine.c
@@ -1,8 +1,22 @@
 #include <ijvm.h>
+#include <util.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int init_ijvm(char *binary_file)
 {
   // Implement loading of binary here
+  FILE *fp;
+  int buf_size = 128;
+  char buffer[buf_size];
+
+  while (sizeof(binary_file) > 0)
+  {
+    fp = open(binary_file, "r");
+    fread(buffer, sizeof(char), buf_size, fp);
+    printf("The file was successfully read.");
+  }
+
   return -1;
 }
 
