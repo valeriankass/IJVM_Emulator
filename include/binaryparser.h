2066b74fac269f33c3ca2c346b432bebb0be4045
valeriankass <lady.komarnickaya@gmail.com>
2020-06-01 16:03:14 +0200

    no warnings, basic init_ijvm

---
+++ include/binaryparser.h
@@ -0,0 +1,10 @@
+#ifndef BINARYPARSER_H
+#define BINARYPARSER_H
+
+#include "ijvm.h"
+#include "util.h"
+
+
+
+
+#endif
--- src/machine.c
+++ src/machine.c
@@ -10,14 +10,17 @@ int init_ijvm(char *binary_file)
   int buf_size = 128;
   char buffer[buf_size];
 
-  while (sizeof(binary_file) > 0)
+  if (sizeof(binary_file) > 0 && (fp = fopen(binary_file, "r")))
   {
-    fp = open(binary_file, "r");
     fread(buffer, sizeof(char), buf_size, fp);
     printf("The file was successfully read.");
   }
-
-  return -1;
+  else
+  {
+    fprintf(stderr, "The file was not successfully read.");
+    return -1;
+  }
+  return 0;
 }
 
 void destroy_ijvm()
